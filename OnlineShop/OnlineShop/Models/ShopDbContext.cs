053ad180d3b8d4ab3e2620448cb2234b0fcbc9fa
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-04-11 18:18:15 +0300

    PasswordRequirments

---
+++ OnlineShop/OnlineShop/Models/ShopDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace OnlineShop.Models
+{
+
+    public class ShopDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ShopDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ShopDbContext Create()
+        {
+            return new ShopDbContext();
+        }
+    }
+}
