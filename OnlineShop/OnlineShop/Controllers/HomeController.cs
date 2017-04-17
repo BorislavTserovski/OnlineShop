a569db36e9c2f3aaec231e88f9d14616a6150ea1
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-04-17 11:56:01 +0300

    AddedSearch

--- OnlineShop/OnlineShop/Controllers/HomeController.cs
+++ OnlineShop/OnlineShop/Controllers/HomeController.cs
@@ -1,30 +1,38 @@
-﻿using System;
+﻿using OnlineShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
-namespace OnlineShop.Controllers
+namespace MVCBlog.Controllers
 {
     public class HomeController : Controller
+
     {
-        public ActionResult Index()
+        private ShopDbContext db = new
+            ShopDbContext();
+        //Get Recipes
+        public ActionResult Index(string Searchby, string search)
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            if (Searchby == "Make")
+            {
+                return View(db.Cars.Where(x => x.Make.Contains(search)).ToList());
+            }
+            else if (Searchby == "Model")
+            {
+                return View(db.Cars.Where(x => x.Model.Contains(search)).ToList());
+            }
+            else if (Searchby == "Category")
+            {
+                return View(db.Cars.Where(c => c.Category.Name.Contains(search)).ToList());
+            }
+            var recipes = db.Cars
+                .OrderByDescending(p => p.DateAdded).Take(3);
+            return View(recipes.ToList());
         }
     }
+
+
 }
